28dab79fd405289df06783cebc4bde12cfcca48b
Hadi Hadi <hadi.net.hadi@gmail.com>
2025-03-12 18:02:26 -0700

    first draft of accounts app

---
+++ src/accounts/urls.py
@@ -0,0 +1,19 @@
+# accounts/urls.py
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import SignUpView, profile_view, dashboard_view, CustomLoginView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', CustomLoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),  # Redirect to login page after logout
+    path('profile/', profile_view, name='profile'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_management.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_management.html'), name='password_change_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_management.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_management.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_management.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_management.html'), name='password_reset_complete'),
+]
