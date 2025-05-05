ffa3892d39fe8ec2b7aeb0ea30b8407a6a5e874d
Hadi Hadi <hadi.net.hadi@gmail.com>
2025-05-05 01:06:38 -0700

    removed email variables in base.py for now

--- src/nfl_pickems/settings/base.py
+++ src/nfl_pickems/settings/base.py
@@ -100,9 +100,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INVITE_CODE = config("INVITE_CODE", default="fallbackcode")
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@pickemleague.com")
+DEFAULT_FROM_EMAIL = 'noreply@pickemleague.com'
