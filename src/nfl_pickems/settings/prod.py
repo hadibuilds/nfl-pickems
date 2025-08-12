dab38d0f080088984f90625bd5e23aa27cb7f3f0
Hadi Hadi <hadi.net.hadi@gmail.com>
2025-08-12 14:43:38 -0700

    fixed prod settings

--- src/nfl_pickems/settings/prod.py
+++ src/nfl_pickems/settings/prod.py
@@ -2,17 +2,23 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['nfl-pickems.onrender.com']
+ALLOWED_HOSTS = [
+    'nfl-pickems-t60y.onrender.com',  
+    'nfl-pickems.onrender.com',       
+    '.onrender.com'                   
+]
 
 CORS_ALLOWED_ORIGINS = [
+    "https://nfl-pickems-t60y.onrender.com",  
     "https://nfl-pickems.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://nfl-pickems-t60y.onrender.com",  
     "https://nfl-pickems.onrender.com",
-]
+]   
 
 SESSION_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SECURE = True
