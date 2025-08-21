901c11a974e86e863bf941a4312e2e54dcdc9d1d
Hadi Hadi <hadi.net.hadi@gmail.com>
2025-08-20 18:17:30 -0700

    minor ui fixes

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -51,7 +51,7 @@ export default function Navbar({ isOpen, setIsOpen }) {
               className="auth-button signup-button"
             >
               <svg xmlns="http://www.w3.org/2000/svg" className="auth-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
               </svg>
               <span>Sign Up</span>
             </Link>
