27d54b510b10716cc961104b1a02d35aca0bf303
Hadi Hadi <hadi.net.hadi@gmail.com>
2025-08-25 10:38:10 -0700

    Update prod.py - Added production email support

--- src/nfl_pickems/settings/prod.py
+++ src/nfl_pickems/settings/prod.py
@@ -24,3 +24,11 @@ SESSION_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SECURE = True
+
+# For production email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # or your email provider
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # your email
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # app password
