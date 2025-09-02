ce0654867c6340c200751b881367667592f41d6f
Hadi Hadi <hadi.net.hadi@gmail.com>
2025-09-01 18:17:49 -0700

    fixed config path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/static/', // ✅ Ensures assets use Django's static prefix
+  base: '/',
   plugins: [
     react(),
     viteStaticCopy({
